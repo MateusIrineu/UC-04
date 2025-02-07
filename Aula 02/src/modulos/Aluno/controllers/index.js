552c514c6b7f85acd1b9dca141b5cff158528dd7
vlimap <valtemir.procopio@gmail.com>
2025-02-06 22:12:43 -0300

    ADD: implement bcrypt for password hashing in AlunoController

--- Aula 02/src/modulos/Aluno/controllers/index.js
+++ Aula 02/src/modulos/Aluno/controllers/index.js
@@ -1,11 +1,13 @@
-import  {alunos } from "../../../config/database.js";
-import { AlunoModel }  from "../models/index.js";
+import { alunos } from "../../../config/database.js";
+import { AlunoModel } from "../models/index.js";
+import bcrypt from "bcrypt";
 
 export class AlunoController {
   // Create - criando o aluno no array alunos
   criar(matricula, nome, email, senha) {
     try {
-      const novoAluno = new AlunoModel(matricula, nome, email, senha);
+      const senhaHash = bcrypt.hashSync(senha, 10);
+      const novoAluno = new AlunoModel(matricula, nome, email, senha = senhaHash );
       alunos.push(novoAluno);
       console.table(novoAluno);
     } catch (error) {
@@ -14,63 +16,64 @@ export class AlunoController {
   }
 
   editar(matricula, novoNome, novoEmail, novaSenha) {
-    // a93421
     try {
       const aluno = alunos.find((aluno) => aluno.getMatricula === matricula);
       if (!aluno) {
         return console.log("Aluno não encontrado!");
       }
-      if (aluno) { 
-        aluno.nome = novoNome || aluno.nome; 
-        aluno.email = novoEmail || aluno.email;
-        aluno.senha = novaSenha || aluno.senha;
-        return console.table(aluno);
-      }
+      aluno.nome = novoNome || aluno.nome;
+      aluno.email = novoEmail || aluno.email;
+      // Se nova senha for informada, criptografa-a
+      aluno.senha = novaSenha ? bcrypt.hashSync(novaSenha, 10) : aluno.senha;
+      
+      return console.table(aluno);
     } catch (error) {
-        console.error("Erro ao tentar atualizar o aluno", error.message);
+      console.error("Erro ao tentar atualizar o aluno", error.message);
     }
   }
+
   deletarPorMatricula(matricula) {
     try {
-        const index = alunos.findIndex((aluno) => aluno.getMatricula === matricula)
-        if(index === -1){
-            return console.log("Aluno não encontrado")
-        }
-        alunos.splice(index, 1)
-        console.log("Aluno excluido com sucesso!")
+      const index = alunos.findIndex((aluno) => aluno.getMatricula === matricula);
+      if (index === -1) {
+        return console.log("Aluno não encontrado");
+      }
+      alunos.splice(index, 1);
+      console.log("Aluno excluido com sucesso!");
     } catch (error) {
-        console.error("Erro ao tentar excluir o aluno", error.message);
+      console.error("Erro ao tentar excluir o aluno", error.message);
     }
   }
+
   deletarTodos() {
     try {
-        alunos.length = 0;
-        console.log("Todos os alunos excluidos!")
+      alunos.length = 0;
+      console.log("Todos os alunos excluidos!");
     } catch (error) {
-        console.error("Erro ao tentar excluir todos os alunos", error.message);
+      console.error("Erro ao tentar excluir todos os alunos", error.message);
     }
   }
+
   listaPorMatricula(matricula) {
     try {
-        const aluno = alunos.find((aluno) => aluno.getMatricula === matricula);
-        if(!aluno){
-            return console.log("Aluno não encontrado")
-        }
-        console.table(aluno)
+      const aluno = alunos.find((aluno) => aluno.getMatricula === matricula);
+      if (!aluno) {
+        return console.log("Aluno não encontrado");
+      }
+      console.table(aluno);
     } catch (error) {
-        console.error("Erro ao tentar exibir o aluno", error.message);
+      console.error("Erro ao tentar exibir o aluno", error.message);
     }
   }
+
   listarTodos() {
     try {
-        if(alunos.length === 0){
-            return console.log("Não existe alunos a serem exibidos!")
-        }
-        console.table(alunos)
+      if (alunos.length === 0) {
+        return console.log("Não existe alunos a serem exibidos!");
+      }
+      console.table(alunos);
     } catch (error) {
-        console.error("Erro ao tentar exibir os alunos", error.message);
+      console.error("Erro ao tentar exibir os alunos", error.message);
     }
   }
 }
-
-// create, update, read(listar todos por id, matricula, cod), delete(todos ou por algo especifico)
